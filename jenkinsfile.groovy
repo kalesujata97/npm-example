fa26b5e918e984240586e04c549a314a4482b435
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2021-05-25 21:17:35 +0530

    feat(semantic-release): Added semantic-release plugin to maintain release notes

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,7 +1,11 @@
 node("master"){
   stage ('Build') {
  
-    git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/kalesujata97/npm-example.git'
+    git url: 'https://github.com/kalesujata97/npm-example.git'
     bat "npm test"
+  }
+
+  stage ('Release') {
+    bat "npm run semantic-release"
   }
 }
