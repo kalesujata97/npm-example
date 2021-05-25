41407d6d1ee0525925cb120a04e18aee59e6b491
Sujata Kale <49482604+kalesujata97@users.noreply.github.com>
2021-05-25 22:00:11 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,11 +1,19 @@
 node("master"){
+    
+  environment {
+        GH_TOKEN  = "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"
+    }
   stage ('Build') {
- 
     git url: 'https://github.com/kalesujata97/npm-example.git'
+    
+    nodejs('node'){
     bat "npm test"
+      }
   }
 
   stage ('Release') {
-    bat "npm run semantic-release"
+      nodejs('node'){
+    bat "npx semantic-release"
+      }
   }
 }
